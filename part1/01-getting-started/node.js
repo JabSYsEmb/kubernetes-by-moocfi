4d805a34f76a6bf4326e22bcf924acb11204323b
JabSysEmb <abduljabbar.sha97@gmail.com>
2023-02-28 14:48:44 +0300

    log output application 1.07

--- part1/01-getting-started/node.js
+++ part1/01-getting-started/node.js
@@ -1,4 +1,22 @@
+const fastify = require('fastify')({logger: true});
 const crypto = require('crypto');
+
+fastify.register(require('@fastify/cors'), (instance) => {
+  return (req, callback) => {
+    const corsOptions = {
+      // This is NOT recommended for production as it enables reflection exploits
+      origin: true
+    };
+
+    // do not include CORS headers for requests from localhost
+    if (/^localhost$/m.test(req.headers.origin)) {
+      corsOptions.origin = false
+    }
+
+    // callback expects two parameters: error and options
+    callback(null, corsOptions)
+  }
+})
 
 const randomness_generator = {
   random: crypto.randomBytes(20).toString('hex'),
@@ -9,4 +27,15 @@ const randomness_generator = {
   }
 }
 
-randomness_generator.log_each_five_sec()
+fastify.get('/', function (_req, reply) {
+  reply.send({ hash : randomness_generator.random, timestamp: new Date() })
+})
+
+// Run the server!
+fastify.listen({ host: '0.0.0.0', port: process.env.PORT ?? 4000 }, function (err, addr) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  console.log(`Server started in port ${addr.split(':').pop()}`);
+})
