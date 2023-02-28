6f5820384f3dfebb1353d2df93a7cd7026cde213
JabSysEmb <abduljabbar.sha97@gmail.com>
2023-02-28 12:29:05 +0300

    configure container for kubernetes

--- part1/02-Project-v0.1/index.js
+++ part1/02-Project-v0.1/index.js
@@ -1,12 +1,39 @@
-const fastify = require('fastify')();
+const fastify = require('fastify')({ logger: true });
+const path    = require('path');
 require('dotenv').config();
 
-fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
+fastify.register(require('@fastify/cors'), (instance) => {
+  return (req, callback) => {
+    const corsOptions = {
+      // This is NOT recommended for production as it enables reflection exploits
+      origin: true
+    };
+
+    // do not include CORS headers for requests from localhost
+    if (/^localhost$/m.test(req.headers.origin)) {
+      corsOptions.origin = false
+    }
+
+    // callback expects two parameters: error and options
+    callback(null, corsOptions)
+  }
+})
+
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/', // optional: default '/'
+})
+
+fastify.get('/:file', function (req, reply) {
+  reply.sendFile(`${req.params.file}`) // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
+
+fastify.get('/', function (req, reply) {
+  reply.send({ hello: 'world' })
 })
 
 // Run the server!
-fastify.listen({ port: process.env.PORT ?? 3000 }, function (err, addr) {
+fastify.listen({ host: '0.0.0.0', port: process.env.PORT ?? 3000 }, function (err, addr) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
